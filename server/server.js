e4f5b0ee6eea7fd7f67889d9bcc0139415d0f93b
Winnie Luk <winniecluk@gmail.com>
2016-10-31 10:10:52 -0700

    set up dependencies and create models

--- server/server.js
+++ server/server.js
@@ -5,10 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+require('dotenv').config();
+require('./config/database.js');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
